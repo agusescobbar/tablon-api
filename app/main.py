7f01f7d550df65a23f97efc0c2e834225c52d79d
agus <panmmnda@gmail.com>
2026-08-13 22:07:25 -0300

    asdasda

--- app/main.py
+++ app/main.py
@@ -21,7 +21,9 @@ app = FastAPI(
 # entre front y back; por eso NO se puede usar allow_origins=["*"].
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173",
+                    "http://127.0.0.1:5173",
+                      "https://tablon-mensajes.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
